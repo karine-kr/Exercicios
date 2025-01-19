4689abda6be8bb4ea8a2d784fc1f42217ac64cba
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-01-19 16:58:20 -0300

    ajsutes nos desafios 2 e 5

--- desafio2.py
+++ desafio2.py
@@ -3,13 +3,17 @@ preco  = float(input("Digite o preço do produto: R$ "))
 quantidade = int(input("Digite a quantidade de produto: "))
 
 if quantidade <= 10:
-    print('O preço total sem desconto, ficando no valor de R${:.2f}'.format(preco))
+    valor_total = (preco * quantidade)
+    print('O preço total sem desconto, ficando no valor total a ser pago é de R${:.2f}'.format(valor_total))
 elif quantidade >= 11 and quantidade <= 20:
     novo = preco - (preco * 10 / 100)
-    print('O preço está com 10% de desconto, ficando no valor de R${:.2f}'.format(novo))
+    valor_total = (novo * quantidade)
+    print('O preço está com 10% de desconto, ficando valor unitário de R${:.2f} e o valor total a ser pago é de R${:.2f}'.format(novo,valor_total))
 elif quantidade >= 21 and quantidade <= 50:
     novo = preco - (preco * 20 / 100)
-    print('O preço está com 20% de desconto, ficando no valor de R${:.2f}'.format(novo))
+    valor_total = (novo * quantidade)
+    print('O preço está com 20% de desconto, ficando no valor de R${:.2f} e o valor total a ser pago é de R${:.2f}'.format(novo,valor_total))
 elif quantidade > 50:
     novo = preco - (preco * 25 / 100)
-    print('O preço está com 25% de desconto, ficando no valor de R${:.2f}'.format(novo))
+    valor_total = (novo * quantidade)
+    print('O preço está com 25% de desconto, ficando no valor de R${:.2f} e o valor total a ser pago é de R${:.2f}'.format(novo,valor_total))
--- desafio5.py
+++ desafio5.py
@@ -4,13 +4,13 @@ valor  = float(input('Digite o valor do imóvel R$: '))
 
 if valor < 30000.00:
     comissao = valor * 10 / 100
-    novo = valor + (valor * 10 / 100)
+    novo = valor + comissao
     print(f"\n{nome}, o valor sem comissão do imóvel é RS {valor:.2f}, \n O valor de 10% da comissão é de R$ {comissao:.2f},\n Ficando um valor total do imóvel de {novo:.2f}".format(valor,comissao,novo))
 elif valor >= 30000.00 and valor < 50000.00:
     comissao = valor * 15 / 100
-    novo = valor + (valor * 15 / 100)
+    novo = valor + comissao
     print(f"\n{nome}, o valor sem comissão do imóvel é RS {valor:.2f}, \n O valor de 15% da comissão é de R$ {comissao:.2f}, \n Ficando um valor total do imóvel de {novo:.2f}".format(valor,comissao,novo))
 elif valor >= 50000.00:
     comissao = valor * 20 / 100
-    novo = valor + (valor * 20 / 100)
-    print(f"\n{nome}, o valor sem comissão do imóvel é RS {valor:.2f}, \n O valor de 20% da comissã3o é de R$ {comissao:.2f}, \n Ficando um valor total do imóvel de {novo:.2f}".format(valor,comissao,novo))
+    novo = valor + comissao
+    print(f"\n{nome}, o valor sem comissão do imóvel é RS {valor:.2f}, \n O valor de 20% da comissão é de R$ {comissao:.2f}, \n Ficando um valor total do imóvel de {novo:.2f}".format(valor,comissao,novo))
