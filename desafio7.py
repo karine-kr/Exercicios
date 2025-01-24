0d7749230ce2b10765b6000418d250ee99f90120
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-01-24 18:29:16 -0300

    ajuste no 7

--- desafio7.py
+++ desafio7.py
@@ -7,6 +7,6 @@ novo_valor1 = valor1
 
 while novo_valor1 <= valor2:
     somatorio = somatorio + novo_valor1
-    novo_valor1 = novo_valor1 + 1
+    novo_valor1 += 1
 
 print ("O valor da  soma é ", somatorio)
