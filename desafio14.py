76299d274baf555af58c05d21616f4393d9dd83b
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-01-26 23:56:31 -0300

    ajuste no desafio14

--- desafio14.py
+++ desafio14.py
@@ -1,11 +1,10 @@
 nomes = []
 
 for valor in range (20):
-    nome = input('Digite o nome: ')
+    nome = input('\nDigite o nome: ')
     nomes.append(nome)
 
 for valor2 in nomes:
     if valor2 == nome:
-        # nomes.remove(nome)
         nomes_sem_repeticao = list(set(nomes))
-        print(f'Os nomes sem repetições são:', nomes_sem_repeticao) 
+        print(f'\nOs nomes sem repetições são:', nomes_sem_repeticao) 
