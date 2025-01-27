aa45040226642378348e626f78fc483feb423956
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-01-26 23:26:36 -0300

    ajuste no desafio9

--- desafio9.py
+++ desafio9.py
@@ -4,8 +4,9 @@ aux = 1
 
 while True: 
     if numero2 != 0:
-        divisao = numero1 % numero2
-        print('O valor do resultado da divisão = ', divisao)
+        quociente = numero1 / numero2
+        resto = numero1 % numero2
+        print('\nO resultado da divisão consiste em:\n\nResto igual a {:.0f} e quociente igual a {:.0f}\n'.format(resto,quociente))
         break
     else:
         numero2 = float(input("Número igual a zero. Por favor, digite um novo número: "))
