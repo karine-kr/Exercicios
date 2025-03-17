893f57ebdc1ac8a452ccb33235a69919a6a24242
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-03-16 23:30:03 -0300

    ajuste no exercicio 14

--- desafio14.py
+++ desafio14.py
@@ -7,4 +7,5 @@ for valor in range (20):
 for valor2 in nomes:
     if valor2 == nome:
         nomes_sem_repeticao = list(set(nomes))
-        print(f'\nOs nomes sem repetições são:', nomes_sem_repeticao) 
+        
+print(f'\nOs nomes sem repetições são:', nomes_sem_repeticao) 
